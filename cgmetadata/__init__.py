275b03854ea2dc314dec242b1a5bd29307aac0b6
Rhet Turnbull <rturnbull@gmail.com>
2023-11-04 08:36:37 -0700

    Updated doc strings

--- cgmetadata/__init__.py
+++ cgmetadata/__init__.py
@@ -1,4 +1,4 @@
-"""Use native Core Graphics API on macOS to access and change image metadata"""
+"""Use native Core Graphics / ImageIO API on macOS to access and change image metadata"""
 
 from ._version import __version__
 from .cgmetadata import MetadataError
